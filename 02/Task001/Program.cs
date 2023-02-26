64a9c978eddb472924d3ba7896e2b9e6e71a347f
Edgar <sorata05@gmail.com>
2023-02-26 23:22:51 +0500

    Homework 02

---
+++ 02/Task001/Program.cs
@@ -0,0 +1,17 @@
+﻿int ReadNumber()
+{
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SecondNumber(int num)
+{
+    int result = ((num / 10) % 10);
+    return result;
+}
+
+Console.Write("Введите трехзначное число - ");
+int number = ReadNumber();
+int index = number.ToString().Length;
+
+if (index < 3 || index > 3) Console.WriteLine("Это не трехзначное число!!!!");
+else Console.WriteLine($"Вторая цифра - {SecondNumber(number)}");

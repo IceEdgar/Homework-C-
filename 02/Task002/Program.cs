7d86aad97d8816d6f8260c5831e066ae62315319
Edgar <sorata05@gmail.com>
2023-02-27 01:15:27 +0500

    Homework 2, Task 2

---
+++ 02/Task002/Program.cs
@@ -0,0 +1,22 @@
+﻿int ReadNumber() //функция ввода числа
+{
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int ThirdNumber(int num)//функция поиска третьей цифры
+{
+    int result = 0;
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    result = (num % 10);
+    return result;
+}
+
+Console.Write("Введите число - ");
+int number = ReadNumber();
+int index = number.ToString().Length;
+
+if (index < 3) Console.WriteLine("НЕТ ТРЕТЬЕЙ ЦИФРЫ");
+else Console.WriteLine($"Третья цифра - {ThirdNumber(number)}");
--- 02/Task003/Program.cs
+++ 02/Task003/Program.cs
@@ -1,17 +1,1 @@
 ﻿
-
-Console.Write("Введите число - ");
-int number = Convert.ToInt32(Console.ReadLine());
-int index = number.ToString().Length;
-
-if (index < 3) Console.WriteLine("НЕТ ТРЕТЬЕЙ ЦИФРЫ");
-else 
-{
-    int result=0;
-while (number>999)
-   {
-        number = number / 10;
-   }
-result=(number%10);  
-Console.WriteLine($"Третья цифра - {result}");
-}

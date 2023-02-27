7091c3626a8bafa803d201c1709cf47d5a504e3f
Edgar <sorata05@gmail.com>
2023-02-28 00:28:26 +0500

    Task 3

--- 02/Task003/Program.cs
+++ 02/Task003/Program.cs
@@ -1,28 +1,29 @@
-﻿int ReadInt(string message)
+﻿int ReadInt()
+{    
+    int number;
+while (true)
 {
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число от 1 до 7: ");
+    if (int.TryParse(Console.ReadLine(), out number))
+        break;
+    Console.WriteLine("Ошибка ввода!");
+}
+return number;
 }
 
-string WorkHoliday(int a)
+int Week(int num)
 {
-    if (a > 0 && a < 8)
+    if (num > 0 && num < 8)
     {
-        if (a == 7 || a == 6)
-        {
-            Console.Write("Под цифрой " + a + " - Выходной");
-        }
-        else
-        {
-            Console.Write("Под цифрой " + a + " - Рабочий");
-        }
+        if (num == 7 || num == 6) Console.Write($"Под цифрой {num} -Ура! Выходной");
+
+        else Console.Write($"Под цифрой {num} - Пора на работу");
     }
-    else
-    {
-        Console.Write("Вы ввели число не в пределах от 1 до 7, поэтому не возможно определить");
-    }
-    return " день.";
+    else Console.Write("Вы ввели число не в пределах от 1 до 7, поэтому не возможно определить");
+
+    return num;
 }
 
-int dayNumber = ReadInt("Введите число от 1 до 7: ");
-Console.WriteLine(WorkHoliday(dayNumber));
+//Console.Write("Введите число от 1 до 7: ");
+int dayNumber = ReadInt();
+Console.WriteLine(Week(dayNumber));

f9a179fb7514ba59cafefe5a2516db35e07f53ef
Edgar <sorata05@gmail.com>
2023-03-02 19:52:15 +0500

    Task 2

--- 03/Task001/Program.cs
+++ 03/Task001/Program.cs
@@ -1,22 +1,22 @@
-﻿ int number;
-    while (true)
-    {
-        Console.Write("Введите пятизначное число - ");
-        if (int.TryParse(Console.ReadLine(), out number))
-            break;
-        Console.WriteLine("Ошибка ввода!");
-    }
-    int index = number.ToString().Length;
-    
-    int index1=number/10000;
-    int index2=(number%10000)/1000;
-    int index3=(number%100)/10;
-    int index4=number%10;
- Console.WriteLine(index4);
-if (index==5)
+﻿int number;
+while (true)
 {
-     if ((index1==index4)&& (index2==index3))
-    Console.Write($"Число {number} -  палиндром");
-        else Console.Write($"Число {number} - не палиндром");
+    Console.Write("Введите пятизначное число - ");
+    if (int.TryParse(Console.ReadLine(), out number))
+        break;
+    Console.WriteLine("Ошибка ввода!");
 }
-    else Console.Write("Вы ввели не пятизначное число");
+int index = number.ToString().Length;
+
+int index1 = number / 10000;
+int index2 = (number % 10000) / 1000;
+int index3 = (number % 100) / 10;
+int index4 = number % 10;
+Console.WriteLine(index4);
+if (index == 5)
+{
+    if ((index1 == index4) && (index2 == index3))
+        Console.Write($"Число {number} -  палиндром");
+    else Console.Write($"Число {number} - не палиндром");
+}
+else Console.Write("Вы ввели не пятизначное число");
---
+++ 03/Task002/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первую координату точки один - ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите вторую координату точки один - ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третью координату точки один - ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первую координату точки два - ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите вторую координату точки два - ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третью координату точки два - ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Pow((Math.Pow(x2 - x1, 2) +
+                         Math.Pow(y2 - y1, 2) +
+                         Math.Pow(z2 - z1, 2) *
+                                   1.0), 0.5);
+Console.WriteLine($"Расстояние между точками = {d}");

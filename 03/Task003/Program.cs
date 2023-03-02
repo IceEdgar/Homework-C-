79b521b4acc61661337ec14d32efac50a5210b90
Edgar <sorata05@gmail.com>
2023-03-02 20:06:57 +0500

    Task 3

---
+++ 03/Task003/Program.cs
@@ -0,0 +1,17 @@
+﻿int number;
+while (true)
+{
+    Console.Write("Введите число - ");
+    if (int.TryParse(Console.ReadLine(), out number))
+        break;
+    Console.WriteLine("Ошибка ввода!");
+}
+int index = 1;
+double kub = 0;
+
+while (index <= number)
+{
+    kub = Math.Pow(index, 3);
+    Console.WriteLine($"Число {kub} - является кубом числа {index}");
+    index++;
+}

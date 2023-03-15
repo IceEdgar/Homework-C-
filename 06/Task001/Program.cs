4c83c41ddffa1484e072e69bbce09a18ebb57136
Edgar <sorata05@gmail.com>
2023-03-16 02:05:24 +0500

    Homework 06

---
+++ 06/Task001/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.Clear();
+int length = InputLengthMassiv("Введите длинну массива ", "Ошибка ввода данных!");
+int[] array = new int[length];
+FillArray(array, length);
+Console.WriteLine($"[{String.Join(",", array)}]");
+int count = 0;
+FindPositiv(array);
+Console.WriteLine($"Количество элементов > 0 -  {count}");
+
+static int InputLengthMassiv(string message, string errormessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errormessage);
+    }
+}
+
+
+void FillArray(int[] collection, int i)
+{
+    int index = 0;
+    while (index < i)
+    {
+        try
+        {
+            Console.Write("Введите число массива ");
+            array[index] = Convert.ToInt32(Console.ReadLine());
+            index++;
+        }
+        catch (Exception exc)
+        {
+            Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        }
+    }
+}
+
+
+int FindPositiv(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
---
+++ 06/Task002/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Clear();
+double k1 = InputNumber("Введите k1: ");
+double b1 = InputNumber("Введите b1: ");
+double k2 = InputNumber("Введите k2: ");
+double b2 = InputNumber("Введите b2: ");
+double x = FindX(b1, b2, k1, k2);
+double y = FindY(b1, k1, x);
+if (b1 == b2 && k1 == k2) Console.WriteLine($"Прямые параллельны");
+else Console.WriteLine($"Пересечение в точке: ({x};{y})");
+
+static double InputNumber(string message)
+{
+    try
+    {
+        Console.Write(message);
+        return (double.Parse(Console.ReadLine() ?? ""));
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return 0;
+    }
+}
+
+double FindX(double b1, double b2, double k1, double k2)
+{
+    x = -(b1 - b2) / (k1 - k2);
+    x = Math.Round(x, 3);
+    return x;
+}
+
+double FindY(double b1, double k1, double x)
+{
+    y = k1 * x + b1;
+    y = Math.Round(y, 3);
+    return y;
+}
+
+
+
